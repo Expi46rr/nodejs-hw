82972c3451a97dc61b2fad55af155016c4325fa5
Denis Gulko <denis.gulko.1998@gmail.com>
2026-06-30 21:00:23 +0300

    node hm

--- src/server.js
+++ src/server.js
@@ -76,6 +76,8 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
