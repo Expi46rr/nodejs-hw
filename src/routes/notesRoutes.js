b1b955a8fe22e0eff9f1e6afdc00fd4cb8249f83
Denis Gulko <denis.gulko.1998@gmail.com>
2026-07-13 22:23:30 +0300

    node hm3

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -2,7 +2,8 @@ import { Router } from "express";
 // import { Note } from "../models/note.js";
 import { createNote, deleteNote, getNoteById, getAllNotes, updateNote } from "../controllers/notesController.js";
 import { celebrate, } from "celebrate";
-import { createNoteSchema, getAllNotesSchema, noteIdSchema, updateNoteSchema } from "../validator/notesValidator.js";
+import {  } from "../validator/notesValidator.js";
+import { createNoteSchema, getAllNotesSchema, noteIdSchema, updateNoteSchema } from "../validations/notesValidator.js";
 
 const router = Router();
 router.get("/notes",celebrate(getAllNotesSchema), getAllNotes);
