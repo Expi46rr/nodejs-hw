97074e8cffb141099e00e37d17e4d03834519aac
Denis Gulko <denis.gulko.1998@gmail.com>
2026-07-23 16:18:05 +0300

    node hm4

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -1,5 +1,5 @@
 import createHttpError from 'http-errors';
-import { Session } from '../models/session';
+import { Session } from '../models/session.js';
 import { User } from '../models/user.js';
 export const authenticate = async (req, res, next) => {
   const { sessionId, accessToken } = req.cookies;
